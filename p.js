f3c8bdd3957439ba21001851aa87227c5af710c9
Harika2124 <122004529+Harika2124@users.noreply.github.com>
2024-03-05 00:46:18 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    // Implement your form submission logic here
+    alert("Form submitted! (This is a placeholder, implement your own logic.)");
+    // Optionally, you can clear the form fields after submission
+    document.getElementById('contactForm').reset();
+}
